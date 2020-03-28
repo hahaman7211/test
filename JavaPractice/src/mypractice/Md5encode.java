8f8266d05aef212f5e312fcd632e579426a2acdf
Jay <Jay@192.168.0.15>
2020-03-28 15:23:56 +0800

    comment edited

--- JavaPractice/src/mypractice/Md5encode.java
+++ JavaPractice/src/mypractice/Md5encode.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 
 public class Md5encode {
 	
+	//test
 	public static void main(String[] args) {
 		
 		char[] hexDigits = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
